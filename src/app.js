fdc4c109b5b65c1b5836d8349668edd07375a58e
asfandtaimoor <asfandtaimoor786@gmail.com>
2025-02-02 09:38:27 +0500

    WIP :  mongo credential updated

--- src/app.js
+++ src/app.js
@@ -4,12 +4,17 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log('Failed to connect to MongoDB', err);
+  });
 
 const app = express();
 const port = 8000;
